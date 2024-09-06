64d1733c9b169fb730e77349f22a03c81624b43e
Matheus Da Nova <mpereiranova@gmail.com>
2024-09-05 22:38:43 -0300

    references

--- script/script.js
+++ script/script.js
@@ -1,1 +1,24 @@
 let optionsButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option_button");
+let fontName = document.getElementById("font_name");
+let fontSize = document.getElementById("font_size");
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("add_link");
+let alignButtons = document.querySelectorAll(".align");
+let spacingButtons = document.querySelectorAll(".spacing");
+let formatButtons = document.querySelectorAll(".format");
+let scriptButtons = document.querySelectorAll(".script");
+
+//List font list
+
+let fontList = [
+    "Arial",
+    "Times New Roman",
+    "Courier New",
+    "Comic Sans MS",
+    "Verdana",
+    "Garamond",
+    "Georgia",
+    "cursive",
+];
+
